8f135f7ead7bd3ee6f79786709142ce1f190eadb
teppei <46420782+pettei47@users.noreply.github.com>
2023-12-26 14:50:49 +0000

    fix test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,7 +63,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 8
-    assert @user.valid?
+    assert_not @user.valid?
   end
 
   test "password should have a minimum length" do
